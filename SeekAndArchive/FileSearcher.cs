e62894b7127c02beb6c310ce4c5963cf2c4840bf
Gergő Ádám <rapture047@gmail.com>
2019-06-14 16:50:59 +0200

    Add project files.

---
+++ SeekAndArchive/FileSearcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SeekAndArchive
+{
+    class FileSearcher
+    {
+
+
+        public IEnumerable<string> Search(string fileName, string directoryPath)
+        {
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            if (!directory.Exists)
+            {
+                throw new ArgumentException("Directoy does not exist");
+            }
+            return from f in SearchInDir(fileName, directory)
+                   select f.FullName;
+        }
+        private IList<FileInfo> SearchInDir(string fileName, DirectoryInfo directory)
+        {
+            var currentDirMatch = directory.GetFiles(fileName);
+            List<FileInfo> fileInfoList = new List<FileInfo>(currentDirMatch);
+
+            if (directory.GetDirectories()!=null)
+            {
+                foreach (var item in directory.GetDirectories())
+                {
+                    fileInfoList.AddRange(SearchInDir(fileName, item));
+                }
+            }
+
+            return fileInfoList;
+
+        }
+    }
+
+}
---
+++ SeekAndArchive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            FileSearcher fileSearcher = new FileSearcher();
+            IEnumerable<string> result = fileSearcher.Search(args[0], args[1]);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+
+    }
+}
